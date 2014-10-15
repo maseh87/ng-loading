244ab9f87c2e1a9335b8cdc97d69e16eb3021822
Mason <maseh87@gmail.com>
2014-10-14 20:06:52 -0700

    created factories

--- app.js
+++ app.js
@@ -2,26 +2,34 @@ angular.module('ng-loading', [])
 
 .config(function($httpProvider) {
   console.log($httpProvider.interceptors);
-  $httpProvider.interceptors.push(function() {
-    return {
-      request: function(config) {
-        console.log(config, 'config');
-        return config;
-      },
-      response: function(response) {
-        console.log(response, 'response');
-        return response;
-      }
-    };
-  });
+  $httpProvider.interceptors.push('requestInterceptor', 'responseInterceptor');
 })
-
 .controller('LoadingController', function($scope, $http) {
   $scope.test = 'test';
   $http({
     method: 'GET',
     url: 'http://www.reddit.com/.json'
   }).then(function(result) {
-    console.log(result.data);
+    console.log(result.data, 'results');
   });
+})
+.factory('requestInterceptor', function() {
+  return {
+    request: function(config) {
+      console.log(config, 'config');
+      return config;
+    }
+  };
+})
+.factory('responseInterceptor', function() {
+  return {
+    request: function(response) {
+      console.log(response, 'response');
+      return response;
+    }
+  };
 });
+
+
+
+
