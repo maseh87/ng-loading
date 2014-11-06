5c0a0577441904734f208ea4dd1c9d99f3775bd9
Mason <maseh87@gmail.com>
2014-11-05 16:22:43 -0800

    updates

--- app.js
+++ app.js
@@ -99,15 +99,25 @@ angular.module('ng-loading', [])
     replace: true,
     template:
       '<div class="box fade-out">' +
-      '<div class="wrapper">' +
-      '<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">' +
-        '<circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>' +
-      '</svg>' +
-      '<div>' +
+        '<div class="wrapper">' +
+          '<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">' +
+            '<circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>' +
+          '</svg>' +
+        '<div>' +
       '</div>'
+
       // '<div class="box fade-out">' +
       //   '<div class="google-loader"></div>' +
       // '</div>'
+
+      // '<div class="box fade-out">' +
+      //   '<div class="wrapper">' +
+      //     '<svg class="spinner" height="50" width="50">' +
+      //       '<circle class="path" cx="25" cy="25.2" r="19.9" fill="none" stroke-width="6" stroke-miterlimit="10" />' +
+      //     '</svg>' +
+      //   '<div>' +
+      // '</div>'
+
   };
   return directive;
 });
