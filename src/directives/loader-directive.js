2d34f889531eb7c2fe7487dc04fef4163abacb8b
Mason <maseh87@gmail.com>
2014-11-08 13:14:27 -0800

    font awesome icons now supported

--- src/directives/loader-directive.js
+++ src/directives/loader-directive.js
@@ -7,8 +7,7 @@ angular.module('directives', [])
 
   var templates = {
     'load-bar-inbox': '<div class="'+ loading.config.overlay.display +' fade-out">' + '<div class="' + loading.config.class +  '"></div>' + '</div>',
-    'spinner': '<div class="'+ loading.config.overlay.display +' fade-out">' + '<div class="svg-wrapper">' + '<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">' + '<circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>' + '</svg>' + '<div>' + '</div>',
-    'fa': '<div class="' + loading.config.overlay.display + ' fade-out">' + '<div class="wrapper">' + '<i class="' + loading.config.icon +  '"></i></div>' + '</div>'
+    'spinner': '<div class="'+ loading.config.overlay.display +' fade-out">' + '<div class="svg-wrapper">' + '<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">' + '<circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>' + '</svg>' + '<div>' + '</div>'
   };
 
   var directive = {
@@ -23,7 +22,7 @@ angular.module('directives', [])
       checkClass = loading.config.icon.slice(0, 2);
 
       if(checkClass === 'fa') {
-        directive.template = templates['fa'];
+        return '<div class="' + loading.config.overlay.display + ' fade-out">' + '<div class="wrapper">' + '<i class="' + loading.config.icon +  '"></i></div>' + '</div>'
       }
 
       return templates[loading.config.class];
