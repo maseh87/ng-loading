01c1d611afe7d7ddd18e98c8b33c9b6409d4b57c
Mason <maseh87@gmail.com>
2014-11-10 13:40:04 -0800

    added paths to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,8 @@
 var gulp = require('gulp'),
     $    = require('gulp-load-plugins')();
+
+
+var paths = {
+  scripts: ['src/directives/loader-directive.js', 'src/services/*.js'],
+  css: ['src/styles/bar-loader.css', 'src/styles/materialSpinner.css']
+};
