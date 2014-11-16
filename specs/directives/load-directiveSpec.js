cc4e1bd3605134f95f721ba1d94567027bf04c60
Lawrence Hunt <lawrence.hunt@gmail.com>
2014-11-16 09:54:59 +0000

    Updated tests

--- specs/directives/load-directiveSpec.js
+++ specs/directives/load-directiveSpec.js
@@ -1,16 +1,20 @@
 describe('load-directive', function() {
-  var $scope, element, isolate;
+  var $scope, $element, isolate;
   beforeEach(module('ngLoading'));
   beforeEach(inject(function($rootScope, $compile) {
     $scope = $rootScope.$new();
-    element = '<loader></loader>';
-    element = $compile(element)($scope);
+    $element = angular.element('<loader></loader>');
+    $compile($element)($scope);
     $scope.$digest();
-    isolate = element.isolateScope();
+    isolate = $element.isolateScope();
   }));
 
   it('should have isolate scope', function() {
     expect(isolate).to.be.an('object');
+  });
+
+  it('should have a child element with a default class of load-bar-inbox', function() {
+    expect($element.children().hasClass('load-bar-inbox')).to.be(true);
   });
 
 });
