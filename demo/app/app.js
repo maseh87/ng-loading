09b387f6edf1572d8c335dafeb90e02583f55e3e
Mason Hargrove <maseh87@gmail.com>
2015-01-07 22:01:37 -0800

    added toolbar

--- demo/app/app.js
+++ demo/app/app.js
@@ -1,39 +1,47 @@
 angular.module('Demo', [
-  'ngLoading',
+  // 'ngLoading',
   'lumx'
 ])
-.config(function(loadingProvider, $httpProvider) {
-  console.log($httpProvider, 'responseInterceptors');
-})
-.controller('LoadingController', function($scope, $http, $timeout, $document, Interceptor) {
-  var body = angular.element($document[0].body);
-  var config = {};
-  // $scope.showLoading = true;
-  $scope.config = {
-    transitionSpeed: '.3s',
-    overlay: {
-      color: '#FEFEFE',
-      opacity: '.3',
-      display: true
-    }
-  };
+// .config(function(loadingProvider, $httpProvider) {
+  // console.log($httpProvider, 'responseInterceptors');
+// })
+.controller('LoadingController', function($scope, $http, $timeout, $document) {
+  // var body = angular.element($document[0].body);
+  // var config = {};
+  // // $scope.showLoading = true;
+  // $scope.config = {
+  //   transitionSpeed: '.3s',
+  //   overlay: {
+  //     color: '#FEFEFE',
+  //     opacity: '.3',
+  //     display: true
+  //   }
+  // };
 
-  $scope.test = function() {
-    if($scope.config.overlay.display === 'true') $scope.config.overlay.display = true;
-    config = angular.copy($scope.config);
-    $http({
-      method: 'GET',
-      url: 'https://www.reddit.com/.json',
-      // showLoading: false,
-      loadingConfig: config
-    }).then(function(result) {
-      // console.log(result.data);
-    });
-    // Interceptor.start();
-    // $timeout(function(){
-    //   Interceptor.end();
-    // }, 3000);
-  };
+  $scope.colors = [
+    { color: 'Red', hex: '#FFFFFF'}, 
+    { color: 'Blue', hex: '#FFFFFF'}
+  ];
+  $scope.classes = [
+    {class: 'spinner'}
+  ];
+
+  // $scope.test = function() {
+  //   if($scope.config.overlay.display === 'true') $scope.config.overlay.display = true;
+  //   config = angular.copy($scope.config);
+  //   $http({
+  //     method: 'GET',
+  //     url: 'https://www.reddit.com/.json',
+  //     // showLoading: false,
+  //     loadingConfig: config
+  //   }).then(function(result) {
+  //     // console.log(result.data);
+  //   });
+  //   // Interceptor.start();
+  //   // $timeout(function(){
+  //   //   Interceptor.end();
+  //   // }, 3000);
+  // };
 
 });
 
